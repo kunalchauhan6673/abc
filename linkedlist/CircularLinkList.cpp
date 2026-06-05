0726c1e9aff7f07649aecd20cc02c51c2b95a0d1
Kunal Chauhan <114133835+kunalchauhan6673@users.noreply.github.com>
2026-06-05 13:45:10 +0530

    Circular link list: create, display, count and insert

---
+++ linkedlist/CircularLinkList.cpp
@@ -0,0 +1,112 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+ public:
+ int data;
+ Node *next;
+};
+Node *head=nullptr;
+Node *last=nullptr;
+
+// 1. Create
+void create(int a[],int size){
+
+ head = new Node;
+ head->data=a[0];
+ head->next=head;
+
+ Node *t;
+ last=head;
+ for(int i=1;i<size;i++){
+     t= new Node;
+     t->data=a[i];
+     t->next=head;
+
+     last->next=t;
+     last=t;
+ }
+}
+
+// 2. Display
+void display(){
+ if(head==nullptr){
+    cout<<"The list is empty!"<<endl;
+    return;
+ }
+ Node *ptr=head;
+  do{
+    cout<<ptr->data<<" ";
+    ptr=ptr->next;
+  }while(ptr!=head);
+  cout<<endl;
+}
+
+// 3. Count
+int countNodes(){
+ if(ptr==nullptr){
+    return 0;
+ }
+ int c=0;
+ do{
+     c++;
+     ptr=ptr->next;
+ }
+ while(ptr!=head);
+ return c;
+}
+
+// 4. Insert at a position
+void insertAtpos(int pos,int data){
+ if(head==nullptr){
+    if(pos == 0)
+    {
+        Node *t = new Node;
+        t->data = data;
+        t->next = t;
+
+        head = last = t;
+    }
+    return;
+ }
+ if(pos<0||pos>countNodes()){
+    cout<<"Invalid index!"<<endl;
+    return;
+ }
+ else{
+    Node *ptr=head;
+    Node *t= new Node;
+    t->data=data;
+
+    if(pos==0){
+        t->next=head;
+        head=t;
+        last->next=t;
+    }else{
+
+    for(int i=0;i<pos-1;i++){
+        ptr=ptr->next;
+    }
+    t->next=ptr->next;
+    ptr->next=t;
+    }
+ }
+}
+
+// 5. delete from a position
+void deleteFrompos(int pos)
+{
+  if(head==nullptr){
+
+  }
+}
+int main(){
+ int a[]={1,2,3,4,5};
+ create(a,5);
+ display();
+ insertAtpos(0,100);
+ insertAtpos(3,10);
+ insertAtpos(6,1000);
+ display();
+ return 0;
+}
+
