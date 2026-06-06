cae2a533a9a9fe979d2e903b95a7a2effa9d8ef3
Kunal Chauhan <114133835+kunalchauhan6673@users.noreply.github.com>
2026-06-06 17:56:11 +0530

    there's a bug, check tom

---
+++ Stack/stackUsinglinkedlist.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+ public:
+ int data;
+ Node *next;
+};
+class Stack{
+ Node *top;
+ public:
+     Stack(){
+         top=nullptr;
+     }
+     ~Stack(){
+      while(top!=nullptr){
+        Node *temp=top;
+        top=top->next;
+        delete temp;
+      }
+     }
+
+    // we generally dont need isFull in linklist implementation because
+    // linklist becomes full only when no more heap memory is available.
+
+    int isEmpty(){
+     return top==nullptr;
+    }
+
+    void push(int data){
+    Node *t= new Node;
+    t->data=data;
+    t->next=top;
+    top=t;
+    }
+    int pop(){
+    if(isEmpty()){
+        cout<<"linklist is empty!"<<endl;
+        return -1;
+    }
+    Node *t=top;
+    int data = top->data;
+    top=top->next;
+    delete t;
+    }
+    void display(){
+    if(isEmpty()){
+        cout<<"Linklist is empty"<<endl;
+        return;
+    }
+    Node *ptr=top;
+    while(ptr!=nullptr){
+        cout<<ptr->data<<endl;
+        ptr=ptr->next;
+    }
+    }
+};
+int main(){
+ Stack s;
+ s.push(10);
+ s.push(20);
+ s.push(30);
+ s.push(40);
+ cout<<"Popped element is: "<<s.pop()<<endl;
+ s.display();
+ return 0;
+}
