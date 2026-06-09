b2c9fb018ceaedf896cfa506c09d03a76707612c
Kunal Chauhan <114133835+kunalchauhan6673@users.noreply.github.com>
2026-06-09 18:04:19 +0530

    completed parenthesis

---
+++ Stack/parenthesis.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    char data;
+    Node *next;
+};
+
+class Stack
+{
+private:
+    Node *top;
+
+public:
+    Stack()
+    {
+        top = nullptr;
+    }
+
+    ~Stack()
+    {
+        while (top != nullptr)
+        {
+            Node *temp = top;
+            top = top->next;
+            delete temp;
+        }
+    }
+
+    bool isEmpty()
+    {
+        return top == nullptr;
+    }
+
+    void push(char data)
+    {
+        Node *t = new Node;
+        t->data = data;
+        t->next = top;
+        top = t;
+    }
+
+    char pop()
+    {
+        if (isEmpty())
+        {
+            return '\0';
+        }
+
+        Node *t = top;
+        char data = t->data;
+
+        top = top->next;
+        delete t;
+
+        return data;
+    }
+
+    void display()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is Empty\n";
+            return;
+        }
+
+        Node *ptr = top;
+
+        while (ptr != nullptr)
+        {
+            cout << ptr->data << endl;
+            ptr = ptr->next;
+        }
+    }
+};
+
+bool isBalanced(string exp)
+{
+    Stack s;
+
+    for (int i = 0; exp[i] != '\0'; i++)
+    {
+        if (exp[i] == '(')
+        {
+            s.push(exp[i]);
+        }
+        else if (exp[i] == ')')
+        {
+            if (s.isEmpty())
+            {
+                return false;
+            }
+
+            s.pop();
+        }
+    }
+
+    return s.isEmpty();
+}
+
+int main()
+{
+    string exp;
+
+    cout << "Enter Expression: ";
+    cin >> exp;
+
+    if (isBalanced(exp))
+    {
+        cout << "Balanced Parentheses\n";
+    }
+    else
+    {
+        cout << "Not Balanced Parentheses\n";
+    }
+
+    return 0;
+}
